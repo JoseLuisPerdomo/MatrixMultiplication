18adce1a901d4f6e873522949fed5671f7fa0a15
JoseLuisPerdomo <145450270+JoseLuisPerdomo@users.noreply.github.com>
2024-10-19 10:30:59 +0100

    C code

---
+++ Code/C/main/mm_benchmark.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <n>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    
+    double** a = malloc(n * sizeof(double*));
+    double** b = malloc(n * sizeof(double*));
+    double** c = malloc(n * sizeof(double*));
+    
+    for (int i = 0; i < n; ++i) {
+        a[i] = malloc(n * sizeof(double));
+        b[i] = malloc(n * sizeof(double));
+        c[i] = malloc(n * sizeof(double));
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            a[i][j] = (double) rand() / RAND_MAX;
+            b[i][j] = (double) rand() / RAND_MAX;
+            c[i][j] = 0.0;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < n; ++k) {
+            for (int j = 0; j < n; ++j) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        free(a[i]);
+        free(b[i]);
+        free(c[i]);
+    }
+    free(a);
+    free(b);
+    free(c);
+
+    return 0;
+}
