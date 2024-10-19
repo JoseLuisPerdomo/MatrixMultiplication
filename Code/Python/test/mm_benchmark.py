70875756d5a20ccaa42ddd88ddb2169be063887b
JosePerdomoDeVega <perdomo.devega@gmail.com>
2024-10-19 12:02:00 +0100

    Python BenchMark

--- Code/Python/test/mm_benchmark.py
+++ Code/Python/test/mm_benchmark.py
@@ -2,8 +2,8 @@ from Code.Python.main.mm_for_benchmark import matrix_multiplication_efficient, m
 
 
 def test_benchmark_mm_efficient(benchmark):
-    benchmark(matrix_multiplication_efficient, [100, 500, 1000])
+    benchmark(matrix_multiplication_efficient, [100, 200, 300, 400, 500])
 
 
 def test_benchmark_mm_inefficient(benchmark):
-    benchmark(matrix_multiplication_inefficient, [100, 500, 1000])
+    benchmark(matrix_multiplication_inefficient, [100, 200, 300, 400, 500])
